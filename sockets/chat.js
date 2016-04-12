eacb48693c2d097173791e180dd98fd9714889c4
Amanda <amanda.pedroso@hotmail.com>
2016-04-12 17:19:41 -0300

    Capitulo 7.5 - implementando um crud

---
+++ sockets/chat.js
@@ -0,0 +1,52 @@
+module.exports = function(io) {
+var crypto = require('crypto')
+, sockets = io.sockets
+, onlines = {}
+;
+// continuação do sockets/chat.js ...
+	//adicionado esse módulo de criptografia para que a conversa se torne mais 
+	//segura e que apenas duas pessoas possam conversar 
+	//são gerados hashes de segurança para cada sala para que não haja "invasões" 
+	//de terceiras pessoas na conversa
+  sockets.on('connection', function (client) {
+    var session = client.handshake.session
+    , usuario = session.usuario;
+
+    onlines[usuario.email] = usuario.email;
+    for (var email in onlines) {
+      client.emit('notify-onlines', email);
+      client.broadcast.emit('notify-onlines', email);
+    }
+
+    client.on('join', function(sala) {
+    	if(!sala) {
+    		var timestamp = new Date().toString()
+    		, md5 = crypto.createHash('md5');
+    		sala = md5.update(timestamp).digest('hex');
+    		console.log('id da sala >' + sala);
+    	}
+    	session.sala = sala;
+    	client.join(sala);
+    });
+
+  	client.on('send-server', function (msg) {
+      var sala = session.sala
+      , data = {email: usuario.email, sala: sala};
+      msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
+      client.broadcast.emit('new-message', data);
+      sockets.in(sala).emit('send-client', msg);
+    });
+
+    client.on('disconnect', function () {
+      var sala = session.sala
+      , msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
+      client.broadcast.emit('notify-offlines', usuario.email);
+      sockets.in(sala).emit('send-client', msg);
+      delete onlines[usuario.email];
+      client.leave(sala);
+    });
+
+
+
+  });
+}
